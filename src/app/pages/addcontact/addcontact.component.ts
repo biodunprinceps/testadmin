6a2083a5b107a2442273d3fc0fcc1acabcd22c6a
biodunprinceps <abiodun.afolabi@princepsfinance.com>
2024-06-20 00:37:53 +0100

    ds

--- src/app/pages/addcontact/addcontact.component.ts
+++ src/app/pages/addcontact/addcontact.component.ts
@@ -8,21 +8,31 @@ import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-addcontact',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule,MatSnackBarModule,HttpClientModule],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+  ],
   templateUrl: './addcontact.component.html',
-  styleUrl: './addcontact.component.scss'
+  styleUrl: './addcontact.component.scss',
 })
-export class AddcontactComponent implements OnInit{
-
-  private accessToken = 'pk.eyJ1Ijoic2l4Zm8iLCJhIjoiY2ttcXVsdnJwMDFtajJ5cGJ3OG54OWhuNCJ9.iWIOYnsyXh0ZI4YQl92Ecg';
+export class AddcontactComponent implements OnInit {
+  private accessToken =
+    'pk.eyJ1Ijoic2l4Zm8iLCJhIjoiY2ttcXVsdnJwMDFtajJ5cGJ3OG54OWhuNCJ9.iWIOYnsyXh0ZI4YQl92Ecg';
   addContactForm!: FormGroup;
   // phoneNumberPattern = '^\+?\d{1,2}?[-\s]?\(?\d{3}\)?[-\s]?\d{3}[-\s]?\d{4}$';
 
-  constructor(private formBuilder: FormBuilder, private localstorage: LocalStorageService, private toastr: ToastrService,private snackBar: MatSnackBar, private http: HttpClient) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private localstorage: LocalStorageService,
+    private toastr: ToastrService,
+    private snackBar: MatSnackBar,
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {
     this.initForm();
@@ -36,43 +46,69 @@ export class AddcontactComponent implements OnInit{
       name: ['', Validators.required],
       phone: ['', [Validators.required, Validators.pattern(/^\d{11}$/)]],
       email: ['', [Validators.required, Validators.email]],
-      addresses: this.formBuilder.array([this.createAddressGroup()], [Validators.required, Validators.maxLength(5)]),
-      longitude: ['',{ updateOn: 'blur' }],
-      latitude: ['',{ updateOn: 'blur' }]
+      addresses: this.formBuilder.array(
+        [this.createAddressGroup()],
+        [Validators.required, Validators.maxLength(5)]
+      ),
+      longitude: ['-2.61132', { updateOn: 'blur' }],
+      latitude: ['54.543434', { updateOn: 'blur' }],
     });
   }
 
   updateLatLngFields() {
-    const addresses = this.addContactForm.get('addresses')?.value as { address: string }[];
-    const promises = addresses.map(({ address }) => this.getLatLngFromAddress(address));
-  
+    const addresses = this.addContactForm.get('addresses')?.value as {
+      address: string;
+    }[];
+
+    const promises = addresses.map(({ address }) =>
+      this.getLatLngFromAddress(address)
+    );
+
+
     Promise.all(promises)
       .then((latLngs) => {
         const longitudeControl = this.addContactForm.get('longitude');
         const latitudeControl = this.addContactForm.get('latitude');
-  
-        longitudeControl?.setValue(latLngs.map((latLng) => latLng.lng));
-        latitudeControl?.setValue(latLngs.map((latLng) => latLng.lat));
+
+        longitudeControl?.setValue(latLngs.map((latLng:any) => latLng.lng));
+        latitudeControl?.setValue(latLngs.map((latLng:any) => latLng.lat));
       })
       .catch((error) => {
         console.error(error);
-        this.snackBar.open('Failed to get latitude and longitude from addresses', 'Close', {
-          duration: 3000
-        });
+        this.snackBar.open(
+          'Failed to get latitude and longitude from addresses',
+          'Close',
+          {
+            duration: 3000,
+          }
+        );
       });
   }
 
   getLatLngFromAddress(address: string) {
-    const apiUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${this.accessToken}`;
+    console.log('Address: ' + address);  //
 
-    return this.http.get<any>(apiUrl).toPromise().then((response) => {
-      if (response.features.length > 0) {
-        const { coordinates } = response.features[0].geometry;
-        return { lat: coordinates[1], lng: coordinates[0] };
-      } else {
-        throw new Error('Failed to get latitude and longitude from address');
-      }
-    });
+    // Check if the address is empty
+    if (address === '') {
+      console.log('Address is empty, skipping API call.');
+      return; // Return early to skip the API call
+    }
+
+    const apiUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+      address
+    )}.json?access_token=${this.accessToken}`;
+
+    return this.http
+      .get<any>(apiUrl)
+      .toPromise()
+      .then((response) => {
+        if (response.features.length > 0) {
+          const { coordinates } = response.features[0].geometry;
+          return { lat: coordinates[1], lng: coordinates[0] };
+        } else {
+          throw new Error('Failed to get latitude and longitude from address');
+        }
+      });
   }
 
   get addresses() {
@@ -81,7 +117,7 @@ export class AddcontactComponent implements OnInit{
 
   createAddressGroup() {
     return this.formBuilder.group({
-      address: ['', Validators.required]
+      address: ['Nigeria', Validators.required],
     });
   }
 
@@ -95,27 +131,32 @@ export class AddcontactComponent implements OnInit{
     this.addresses.removeAt(index);
   }
 
-
   onSubmit() {
     if (this.addContactForm.valid) {
-      const addresses = this.addContactForm.get('addresses')?.value as { address: string }[];
-      const promises = addresses.map(({ address }) => this.getLatLngFromAddress(address));
-  
+      const addresses = this.addContactForm.get('addresses')?.value as {
+        address: string;
+      }[];
+      const promises = addresses.map(({ address }) =>
+        this.getLatLngFromAddress(address)
+      );
+
       Promise.all(promises)
         .then((latLngs) => {
           const formValue = this.addContactForm.value;
-          formValue.longitude = latLngs.map((latLng) => latLng.lng);
-          formValue.latitude = latLngs.map((latLng) => latLng.lat);
-  
+          formValue.longitude = latLngs.map((latLng:any) => latLng.lng);
+          formValue.latitude = latLngs.map((latLng:any) => latLng.lat);
+
           const existingContacts: any = this.localstorage.getItem('contacts');
-          const contactsArray = existingContacts ? JSON.parse(existingContacts) : [];
+          const contactsArray = existingContacts
+            ? JSON.parse(existingContacts)
+            : [];
           contactsArray.push(formValue);
           this.localstorage.setItem('contacts', JSON.stringify(contactsArray));
-  
+
           this.snackBar.open('Contact added successfully!', 'Close', {
-            duration: 3000
+            duration: 3000,
           });
-  
+
           // Reset the form
           this.addContactForm.reset();
           this.addContactForm.markAsPristine();
@@ -125,15 +166,19 @@ export class AddcontactComponent implements OnInit{
         })
         .catch((error) => {
           console.error(error);
-          this.snackBar.open('Failed to get latitude and longitude from addresses', 'Close', {
-            duration: 3000
-          });
+          this.snackBar.open(
+            'Failed to get latitude and longitude from addresses',
+            'Close',
+            {
+              duration: 3000,
+            }
+          );
         });
     } else {
       this.showFormErrors();
     }
   }
-  
+
   showFormErrors() {
     for (const key in this.addContactForm.controls) {
       if (this.addContactForm.controls.hasOwnProperty(key)) {
@@ -143,7 +188,7 @@ export class AddcontactComponent implements OnInit{
         control.markAsTouched();
 
         if (control instanceof FormArray) {
-          (control as FormArray).controls.forEach(group => {
+          (control as FormArray).controls.forEach((group) => {
             for (const groupKey in (group as FormGroup).controls) {
               if ((group as FormGroup).controls.hasOwnProperty(groupKey)) {
                 const groupControl = (group as FormGroup).controls[groupKey];
@@ -158,8 +203,7 @@ export class AddcontactComponent implements OnInit{
 
     // Show an error snackbar notification
     this.snackBar.open('Please correct the highlighted errors.', 'Close', {
-      duration: 3000
+      duration: 3000,
     });
   }
-  
 }
